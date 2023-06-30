adb67f966d1b49be16e4d1993f66367d241756f3
lllilyk <ym0402kkk@naver.com>
2023-06-30 09:57:46 +0900

    bookJAC's first commit

---
+++ src/main/java/com/example/bookjac/BookJacApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookjac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookJacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookjac/BookJacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookjac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
